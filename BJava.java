684deadef7e5c4cf9db64d92e73b44b63aaf81f7
atsta <atstamatiou@gmail.com>
2020-12-19 15:27:07 +0200

    java slower

--- BJava.java
+++ BJava.java
@@ -20,66 +20,35 @@ class List {
         Node newNode = new Node(this.head, newdata);
         this.head = newNode;
     }
-
-    void display() {
-        Node ptr;
-        ptr = this.head;
-        while (ptr != null) {
-            System.out.print(ptr.data[0] + " ");
-            ptr = ptr.next;
-        }
-        System.out.println(" ");
-    }
 }
 
 public class BJava {
     public static void main(String[] args) {
         int i, j, count, back, front;
-        int items = 10000000, data_items = 150;
+        int items = 10000000, data_items = 30;
+
+        //count time taken for operations in the final "alive" list
+        long startTime = System.nanoTime();
 
         List l1 = new List();
         for (i = 0;i <= items; i++) {
             int [] newdata = new int[data_items];
-            for (j = 0;j < 150;j++) {
+            for (j = 0;j < data_items;j++) {
                 newdata[j] = i;
             }
             l1.insert(newdata);
         }
         l1.head = null;
 
-        List l2 = new List();
-        for (i = 0;i <= items; i++) {
-            int [] newdata = new int[data_items];
-            for (j = 0;j < 150;j++) {
-                newdata[j] = i;
-            }
-            l2.insert(newdata);
-        }
-        l2.head = null;
-
-        List l3 = new List();
-        for (i = 0;i <= items; i++) {
-            int [] newdata = new int[data_items];
-            for (j = 0;j < 150;j++) {
-                newdata[j] = i;
-            }
-            l3.insert(newdata);
-        }
-
-        l3.head = null;
-
         List l4 = new List();
 
         for (i = 0;i <= items; i++) {
             int [] newdata = new int[data_items];
-            for (j = 0;j < 150;j++) {
+            for (j = 0;j < data_items;j++) {
                 newdata[j] = i;
             }
             l4.insert(newdata);
         }
-
-        //count time taken for operations in the final "alive" list
-        long startTime = System.nanoTime();
 
         Node ptr;
         ptr = l4.head;
@@ -96,12 +65,72 @@ public class BJava {
             ptr = ptr.next;
         }
 
+        List l2 = new List();
+        for (i = 0;i <= items; i++) {
+            int [] newdata = new int[data_items];
+            for (j = 0;j < data_items;j++) {
+                newdata[j] = i;
+            }
+            l2.insert(newdata);
+        }
+        l2.head = null;
+
         ptr = l4.head;
         count = 0;
         while (ptr != null) {
             count++;
             ptr = ptr.next;
         }
+
+        List l3 = new List();
+        for (i = 0;i <= items; i++) {
+            int [] newdata = new int[data_items];
+            for (j = 0;j < data_items;j++) {
+                newdata[j] = i;
+            }
+            l3.insert(newdata);
+        }
+
+        l3.head = null;
+
+        ptr = l4.head;
+        count = 0;
+        while (ptr != null) {
+            count++;
+            ptr = ptr.next;
+        }
+
+        List l5 = new List();
+        for (i = 0;i <= items; i++) {
+            int [] newdata = new int[data_items];
+            for (j = 0;j < data_items;j++) {
+                newdata[j] = i;
+            }
+            l5.insert(newdata);
+        }
+
+        l5.head = null;
+
+        ptr = l4.head;
+        count = 0;
+        while (ptr != null) {
+            count++;
+            ptr = ptr.next;
+        }
+
+        /*for (i = 0;i < 10;i++) {
+            List l5 = new List();
+            for (i = 0;i <= items; i++) {
+                int [] newdata = new int[data_items];
+                for (j = 0;j < data_items;j++) {
+                    newdata[j] = i;
+                }
+                l5.insert(newdata);
+            }
+
+            l5.head = null;
+
+        } */
 
         System.out.println(sum);
         System.out.println(count);
